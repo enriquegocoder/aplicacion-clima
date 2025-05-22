f8b2aecaa5d5672ebac6741ef84d0c5bc3a5bb77
Enrique Gonzalez Duque <63886300+enriquegocoder@users.noreply.github.com>
2025-05-22 18:32:35 -0500

    Icono al inicio

--- src/WheatherApp.jsx
+++ src/WheatherApp.jsx
@@ -29,7 +29,7 @@ export const WheatherApp = () => {
         } catch (error) {
             console.error(error);
         }
-    }       
+    }
 
     return (
         <div className="container">
@@ -46,7 +46,19 @@ export const WheatherApp = () => {
             </form>
             {dataClima && (
                 <div>
-                    <h2>{dataClima.name}</h2>
+                    
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-6">
+                                <span class="text-inline"><h2>{dataClima.name}</h2></span>
+                            </div>
+                            <div class="col-6">
+                                <img src={`https://openweathermap.org/img/wn/${dataClima.weather[0].icon}@2x.png`} class="img-fluid" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <p>País: {dataClima.sys.country}</p>
                     <p>Temperatura: {dataClima.main.temp} °C</p>
                     <p>Descripción: {dataClima.weather[0].description}</p>
                     <p>Humedad: {dataClima.main.humidity} %</p>
@@ -63,9 +75,9 @@ export const WheatherApp = () => {
                     <p>Zona horaria: {dataClima.timezone / 3600} horas</p>
                     <p>Condiciones del mar: {dataClima.main.sea_level} hPa</p>
                     <p>Condiciones de la tierra: {dataClima.main.grnd_level} hPa</p>
-                    <img src={`https://openweathermap.org/img/wn/${dataClima.weather[0].icon}@2x.png`} />
+                    {/* <img src={`https://openweathermap.org/img/wn/${dataClima.weather[0].icon}@2x.png`} /> */}
                 </div>
-            )}      
+            )}
         </div>
     )
 }
